9781945bfed7f483e67845fa9633b9fd7467aa92
Chandrashekar-Akarsh <101552948+Chandrashekar-Akarsh@users.noreply.github.com>
2025-01-19 14:59:45 -0600

    understanding basics of react

--- App.js
+++ App.js
@@ -1,3 +1,35 @@
-const heading = React.createElement("h1", {}, "Hello World From React");
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading1" },
+//   "Hello World From React"
+// );
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+// console.log(heading);
+
+{
+  /* <div id="parent">
+  <div id="child">
+    <h1>NOICE</h1>
+    <h2>This is Sibling of NOICE</h2>
+  </div>
+</div> */
+}
+
+const heading = React.createElement("h1", {}, "NOICE");
+const heading2 = React.createElement("h2", {}, "This is Sibling of NOICE");
+const div = React.createElement("div", { id: "child" }, [heading, heading2]);
+const parentdiv = React.createElement("div", { id: "parent" }, div);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parentdiv);
+
+const elementToRender = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "NOICE"),
+    React.createElement("h2", {}, "This is Sibling of NOICE"),
+  ])
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(elementToRender);
